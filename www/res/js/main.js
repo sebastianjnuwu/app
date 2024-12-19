107de3e2e4ca410a95922fd5c45682580edc9bf2
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-12-19 15:31:43 -0300

    update www/res/js/main.js

--- www/res/js/main.js
+++ www/res/js/main.js
@@ -1,1 +1,13 @@
-console.log("Hooy!");
+import { AdMob } from '@capacitor-community/admob';
+
+AdMob.initialize();
+
+const options = {
+  adId: 'ca-app-pub-6690516270288705/1043067086', 
+  adSize: 'BANNER',
+  position: 'BOTTOM_CENTER',
+  margin: 0,
+  isTesting: true 
+};
+
+AdMob.showBanner(options);
