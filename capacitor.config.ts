4acd3746c3c20a1408d87a6960200d7f6a7d7ab2
sebastianjnuwu <sebastianjnuwu@gmail.com>
2024-12-19 13:33:24 -0300

    v1.0.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,14 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'uwu.cookie.app',         
-  appName: 'Cookie',               
-  webDir: 'www',                   
-  bundledWebRuntime: false         
+  appId: 'uwu.cookie.app',
+  appName: 'Cookie',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  android: {
+    backgroundColor: '#FFFFFF',
+    appendUserAgent: 'Cookie' 
+  }
 };
 
 export default config;
