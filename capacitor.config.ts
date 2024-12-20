ff73a7087115576ac4e3a436717551e09c1f2985
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-12-19 22:12:40 -0300

    update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'uwu.cookie.app',
   appName: 'Cookie',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    AdMob: {
+      androidAppId: 'ca-app-pub-6690516270288705/1043067086',
+      maxAdContentRating: 'G', 
+      tagForChildDirectedTreatment: false, 
+      tagForUnderAgeOfConsent: false,
+    },
+  },
 };
 
 export default config;
