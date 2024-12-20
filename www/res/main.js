adf70d439214328bb489408f67304b35f02cf164
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-12-20 08:29:04 -0300

    update www/res/main.js

--- www/res/main.js
+++ www/res/main.js
@@ -1,37 +1,47 @@
-console.log("Okkk.....");
+const { AdMob } = window.Capacitor.Plugins;
 
-import { AdMob, BannerAdOptions, BannerAdSize, BannerAdPosition } from '@capacitor-community/admob';
-
-document.addEventListener('deviceready', async () => {
-  try {
-    await AdMob.initialize();
-    console.log('AdMob inicializado com sucesso.');
-  } catch (error) {
-    console.error('Erro ao inicializar o AdMob:', error);
-  }
+AdMob.initialize().then(() => {
+  console.log('AdMob inicializado...');
+}).catch((err) => {
+  console.error(err);
 });
 
- const showBanner = async () => {
-  const options = {
-    adId: 'ca-app-pub-6690516270288705/1043067086', 
-    adSize: BannerAdSize.BANNER,
-    position: BannerAdPosition.BOTTOM_CENTER,
-    margin: 0,
-  };
-
-  try {
-    await AdMob.showBanner(options);
-    console.log('Banner exibido com sucesso.');
-  } catch (error) {
-    console.error('Erro ao exibir o banner:', error);
-  }
+function a() {
+AdMob.showBanner({
+  adId: 'ca-app-pub-6690516270288705/1043067086', 
+  adSize: 'BANNER',
+  position: 'BOTTOM_CENTER',
+  margin: 0,
+}).then(() => {
+  console.log('Banner exibido..');
+}).catch((err) => {
+  console.error(err);
+});
 };
 
-const removeBanner = async () => {
-  try {
-    await AdMob.removeBanner();
-    console.log('Banner removido com sucesso.');
-  } catch (error) {
-    console.error('Erro ao remover o banner:', error);
-  }
+function b() {
+  AdMob.hideBanner()
+  .then(() => {
+    console.log('Banner ocultado com sucesso.');
+  })
+  .catch((error) => {
+    console.error('Erro ao ocultar o banner:', error);
+  });
+
+};
+
+function c() {
+  const adId = 'ca-app-pub-6690516270288705/2597759707'; // ID do bloco real
+  AdMob.prepareRewardVideoAd({ adId })
+    .then(() => {
+      console.log('Intersticial recompensado preparado.');
+      return AdMob.showRewardVideoAd();
+    })
+    .then(() => {
+      console.log('Intersticial recompensado exibido com sucesso.');
+      // Aqui você pode adicionar a lógica para entregar a recompensa ao usuário
+    })
+    .catch((err) => {
+      console.error('Erro ao exibir intersticial recompensado:', err);
+    });
 };
