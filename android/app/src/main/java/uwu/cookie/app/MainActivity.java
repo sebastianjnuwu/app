78458ddd9187465b13de131b2ece3d9c806a9568
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-12-19 21:36:13 -0300

    Update MainActivity.java

--- android/app/src/main/java/uwu/cookie/app/MainActivity.java
+++ android/app/src/main/java/uwu/cookie/app/MainActivity.java
@@ -1,22 +1,5 @@
 package uwu.cookie.app;
 
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-                System.out.println("AdMob Initialized!");
-            }
-        });
-    }
-}
+public class MainActivity extends BridgeActivity {}
