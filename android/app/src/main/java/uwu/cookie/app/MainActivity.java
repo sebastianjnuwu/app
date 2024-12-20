bedc53144013833a9c64da7fbf23364195bc8f5b
sebastianjnuwu <sebastianjnuwu@gmail.com>
2024-12-19 21:13:38 -0300

    v1.0.0 - 2

--- android/app/src/main/java/uwu/cookie/app/MainActivity.java
+++ android/app/src/main/java/uwu/cookie/app/MainActivity.java
@@ -1,5 +1,19 @@
 package uwu.cookie.app;
 
-import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends AppCompatActivity {
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    new Thread(
+            () -> {
+              // Initialize the Google Mobile Ads SDK on a background thread.
+              MobileAds.initialize(this, initializationStatus -> {});
+            })
+        .start();
+  }
+}
