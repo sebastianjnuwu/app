3fdc1f50db993e54655538ef25b35be5b76674a2
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2024-12-19 21:50:43 -0300

    update android/app/src/main/java/uwu/cookie/app/MainActivity.java

--- android/app/src/main/java/uwu/cookie/app/MainActivity.java
+++ android/app/src/main/java/uwu/cookie/app/MainActivity.java
@@ -1,5 +1,13 @@
 package uwu.cookie.app;
 
 import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
+import app.xplatform.capacitor.plugins.AdMob;
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      add(AdMob.class);  // Add AdMob as a Capacitor Plugin
+    }});
+  }
+}
