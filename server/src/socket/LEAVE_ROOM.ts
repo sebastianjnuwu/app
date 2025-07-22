9788f168c3ae353ccbfd578637bcabd94d7cea13
sebastianjnuwu <sebastianjnuwu@gmail.com>
2025-07-22 20:08:09 +0000

    feat(back-end): Leave Romm - bug
    by @sebastianjnuwu

--- server/src/socket/LEAVE_ROOM.ts
+++ server/src/socket/LEAVE_ROOM.ts
@@ -5,23 +5,32 @@ import chalk from "chalk";
 import type { Socket } from "socket.io";
 
 export async function LEAVE_ROOM({ USER, ROOM_CODE }: LeaveRoomData) {
-
+  
   const socket = this as Socket;
 
-  const ROOM = await db.ROOM.findUnique({
+  let ROOM = await db.ROOM.findUnique({
     where: { CODE: ROOM_CODE },
-    include: { OWNER: true }
+    include: {
+     OWNER: true,
+     PLAYERS: {
+      where: {
+        UID: { not: USER.UID }
+      }
+     }
+    }
   });
-
+  
   if (!ROOM) return;
-
+  
   if (ROOM.STATE !== "WAITING") {
+    
     await db.PLAYER.update({
       where: { UID: USER.UID },
       data: { ROOM_ID: null }
     });
 
     logger.info(`🚪 Player ${chalk.green(`"${USER.NAME}"`)} left room ${chalk.cyanBright(`"${ROOM_CODE}"`)} with state: ${chalk.yellow(ROOM.STATE)}`);
+    
     socket.leave(ROOM_CODE);
     return;
   }
@@ -31,56 +40,35 @@ export async function LEAVE_ROOM({ USER, ROOM_CODE }: LeaveRoomData) {
     data: { ROOM_ID: null }
   });
 
-  const REMAINING_PLAYERS = await db.PLAYER.findMany({
-    where: { ROOM_ID: ROOM.ID }
-  });
-
-  if (REMAINING_PLAYERS.length === 0) {
-    await db.ROOM.delete({
-      where: { ID: ROOM.ID }
+  if (ROOM.PLAYERS.length === 0) {
+    
+    await db.ROOM.delete({ 
+      where: { ID: ROOM.ID } 
     });
 
     logger.info(`🗑️ Room ${chalk.cyanBright(`"${ROOM_CODE}"`)} deleted. Last owner was ${chalk.green(`"${USER.NAME}"`)}.`);
+    
     socket.leave(ROOM_CODE);
     return;
-  }
+  };
 
-  if (ROOM.OWNER_ID === USER.ID) {
-    const NEW_OWNER = REMAINING_PLAYERS[0];
+  if (ROOM.OWNER.UID === USER.UID) {
+
+    const NEW_OWNER = ROOM.PLAYERS[0];
 
     await db.ROOM.update({
       where: { ID: ROOM.ID },
       data: { OWNER_ID: NEW_OWNER.ID }
     });
 
-    const UPDATED_ROOM = await db.ROOM.findUnique({
-      where: { ID: ROOM.ID },
-      include: { PLAYERS: true, OWNER: true }
-    });
-
     socket.to(ROOM_CODE).emit("UPDATE_ROOM", {
       TYPE: "OWNER_CHANGED",
-      ROOM: safeRoom(UPDATED_ROOM),
-      NEW_OWNER: safePlayer(NEW_OWNER)
+      OLD_OWNER: USER.NAME,
+      NEW_OWNER: NEW_OWNER.NAME,
+      ROOM: safeRoom(ROOM),
     });
 
-    socket.to(ROOM_CODE).emit("UPDATE_ROOM", {
-      TYPE: "LEAVE",
-      PLAYER: safePlayer(USER),
-      ROOM: safeRoom(UPDATED_ROOM)
-    });
-  }
-
-  const UPDATED_ROOM = await db.ROOM.findUnique({
-    where: { ID: ROOM.ID },
-    include: { PLAYERS: true, OWNER: true }
-  });
-
-  socket.to(ROOM_CODE).emit("UPDATE_ROOM", {
-    TYPE: "LEAVE",
-    PLAYER: safePlayer(USER),
-    ROOM: safeRoom(UPDATED_ROOM)
-  });
+  };
 
   socket.leave(ROOM_CODE);
 
